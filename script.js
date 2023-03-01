0452eddb3ff6ef4bd7af696216e9f79d70ee447d
nouvelleapprenante <aprons.toiler05@icloud.com>
2023-03-01 19:25:58 +0530

    Add sum functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let grid = document.querySelector("#grid");
 
 let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
 let screen = document.querySelector("#numbers");
-
+let count = 0;
 let a = "";
 let b = "";
 
@@ -20,13 +20,28 @@ const calculate = (operator) => {
     }
 }
 
+
 numbers.forEach((e)=>{
     let nums = document.createElement('button');
     nums.innerText = e;
     // add the numbers and operators to the input screen
     nums.addEventListener('click',()=>{
+        if(e!="="){
         screen.value += e;
+        }
+        if (e == "+"){
+            count+=1;
+        }
+        if (count>=2){
+            let problem = screen.value.split("+");
+            a = Number(problem[0]);
+            b = Number(problem[1]);
+            screen.value = calculate("+");
+            
+        }
+
     })
+
     grid.appendChild(nums);
 })
 
