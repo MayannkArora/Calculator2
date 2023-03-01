f3c0dfe9eb92cb91a2c3858e52b02f309e41fb02
nouvelleapprenante <aprons.toiler05@icloud.com>
2023-03-01 18:37:04 +0530

    Add event listeners to enter numbers and operators

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let grid = document.querySelector("#grid");
 
 let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
+let screen = document.querySelector("#numbers");
 
 numbers.forEach((e)=>{
     let nums = document.createElement('button');
     nums.innerText = e;
+    nums.addEventListener('click',()=>{
+        screen.value += e;
+    })
     grid.appendChild(nums);
+    
 })
+
