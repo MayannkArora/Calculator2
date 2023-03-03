3252962da4c8b7ea444f7c24a38e5c11d74ce403
nouvelleapprenante <aprons.toiler05@icloud.com>
2023-03-04 00:43:58 +0530

    Reset operator function due to errors

--- script.js
+++ script.js
@@ -1,66 +1,9 @@
 let grid = document.querySelector("#grid");
-let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
-let screen = document.querySelector("#numbers");
-let count = 0;
-let a = "";
-let b = "";
 
-const calculate = (operator) => {
-    a = parseInt(a);
-    b = parseInt(b);
-    if (operator=="+"){
-        return a+b;
-    } else if (operator == "-"){
-        return a-b;
-    } else if (operator == "*"){
-        return a*b;
-    } else {
-        return a/b;
-    }
-}
+ let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
 
-numbers.forEach((e)=>{
-    let nums = document.createElement('button');
-    nums.innerText = e;
-    // add the numbers and operators to the input screen
-    nums.addEventListener('click',()=>{
-        if(e!="="){
-        screen.value += e;
-        }
-        if (e == "+" || e=="-" || e=="*"|| e=="/" || e=="="){
-            count+=1;
-        }
-        if (count>=2){
-            if(e == "+"){
-            let problem = screen.value.split("+");
-            a = Number(problem[0]);
-            b = Number(problem[1]);
-            screen.value = calculate("+")+"+";
-            count=0;           
-        }
-        if(e == "-"){
-            let problem = screen.value.split("-");
-            a = Number(problem[0]);
-            b = Number(problem[1]);
-            screen.value = calculate("-")+"-";
-            count=1;           
-        }
-        if(e == "*"){
-            let problem = screen.value.split("*");
-            a = Number(problem[0]);
-            b = Number(problem[1]);
-            screen.value = calculate("*")+"*";
-            count=1;           
-        }
-        if(e == "/"){
-            let problem = screen.value.split("/");
-            a = Number(problem[0]);
-            b = Number(problem[1]);
-            screen.value = calculate("/")+"/";
-            count=1;           
-        }
-    }
-    })
-    grid.appendChild(nums);
-})
-
+ numbers.forEach((e)=>{
+     let nums = document.createElement('button');
+     nums.innerText = e;
+     grid.appendChild(nums);
+ })
