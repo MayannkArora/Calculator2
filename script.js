a4c6e0804359d0907edfe8d39449e017b81f44d7
nouvelleapprenante <aprons.toiler05@icloud.com>
2023-03-05 23:03:07 +0530

    Add event listeners

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let grid = document.querySelector("#grid");
+let screen = document.querySelector("#numbers");
+let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
+let count = 1;
+let calculate = [];
+let eval = false;
 
- let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
-
- numbers.forEach((e)=>{
-     let nums = document.createElement('button');
-     nums.innerText = e;
-     grid.appendChild(nums);
- })
+numbers.forEach((e)=>{
+    let nums = document.createElement('button');
+    nums.innerText = e;
+    grid.appendChild(nums);
+    nums.addEventListener('click',()=>{
+        if(e!="+" && e!="*" && e!="/" && count==1){
+            screen.value += e;
+        }
+    })
+})
