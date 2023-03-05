1615bb334c6657013e2a0aa42e61d6f4a3d0f2b6
nouvelleapprenante <aprons.toiler05@icloud.com>
2023-03-06 00:12:33 +0530

    Add addition and subtraction funcationality

--- script.js
+++ script.js
@@ -11,7 +11,47 @@ numbers.forEach((e)=>{
     grid.appendChild(nums);
     nums.addEventListener('click',()=>{
         if(e!="+" && e!="*" && e!="/" && count==1){
+            if(screen.value.length==0){
+                screen.value+=e;
+            } else if(screen.value.length>=1){
+                if(e=="-"){
+                calculate[0] = screen.value;
+                calculate[1] = e;
+                screen.value += e;
+                count += 1;
+                console.log(calculate);
+                } else if (e!="-"){
+                    screen.value+=e;
+                }
+            }
+        } else if (count ==1 && screen.value.length>=1 && (e=="-" || e=="+" || e=="/" || e=="*")){
+            calculate[0] = screen.value;
+            calculate[1] = e;
             screen.value += e;
+            count += 1;
+            console.log(calculate);
+        } else if(count == 2){
+            if(e!="+" && e!="*" && e!="/" && e!="-"){
+                screen.value+=e;
+            } else{
+                let sign = calculate[1];
+                let divide = screen.value.split(sign);
+                if(sign!="-"){
+                    calculate[2] = divide[1];
+                } else {
+                    calculate[2] = divide[2];
+                }
+                if(sign=="+"){
+                    let sum = parseInt(calculate[0]) + parseInt(calculate[2]);
+                    screen.value = sum;
+                    screen.value += e;
+                } else if(sign=="-"){
+                    let difference = parseInt(calculate[0])-parseInt(calculate[2]);
+                    screen.value = difference;
+                    screen.value += e;
+                }
+            }
+            
         }
     })
 })
