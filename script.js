03829ebffbee62b70e85ea4ac2df49a299cd4c73
nouvelleapprenante <aprons.toiler05@icloud.com>
2023-03-06 02:10:42 +0530

    Completely revamp the project

--- script.js
+++ script.js
@@ -4,54 +4,46 @@ let numbers = [1,2,3,4,5,6,7,8,9,"+",0,"-","/","*","="];
 let count = 1;
 let calculate = [];
 let eval = false;
+let num1 = document.querySelector("#num1");
+num1.value="";
+let num2 = document.querySelector("#num2");
+num2.value="";
+let sign = document.querySelector("#sign");
+sign.value="";
+
+
+
+const operate = (num1,num2,sign) =>{
+    if (sign=="+"){
+        return num1+num2
+    } else if (sign=="-"){
+        return num1-num2
+    } else if (sign=="*"){
+        return num1*num2
+    } else if (sign=="/"){
+        return num1/num2
+    }
+}
+
+const reset = (e) =>{
+    e.value="";
+} 
+
+const updateNum1 = (e) => {
+        num1.innerText += e;
+}
+
+const updateNum2 = (e) =>{
+    num2.innerText += e;
+}
+
 
 numbers.forEach((e)=>{
     let nums = document.createElement('button');
     nums.innerText = e;
     grid.appendChild(nums);
     nums.addEventListener('click',()=>{
-        if(e!="+" && e!="*" && e!="/" && count==1){
-            if(screen.value.length==0){
-                screen.value+=e;
-            } else if(screen.value.length>=1){
-                if(e=="-"){
-                calculate[0] = screen.value;
-                calculate[1] = e;
-                screen.value += e;
-                count += 1;
-                console.log(calculate);
-                } else if (e!="-"){
-                    screen.value+=e;
-                }
-            }
-        } else if (count ==1 && screen.value.length>=1 && (e=="-" || e=="+" || e=="/" || e=="*")){
-            calculate[0] = screen.value;
-            calculate[1] = e;
-            screen.value += e;
-            count += 1;
-            console.log(calculate);
-        } else if(count == 2){
-            if(e!="+" && e!="*" && e!="/" && e!="-"){
-                screen.value+=e;
-            } else{
-                let sign = calculate[1];
-                let divide = screen.value.split(sign);
-                if(sign!="-"){
-                    calculate[2] = divide[1];
-                } else {
-                    calculate[2] = divide[2];
-                }
-                if(sign=="+"){
-                    let sum = parseInt(calculate[0]) + parseInt(calculate[2]);
-                    screen.value = sum;
-                    screen.value += e;
-                } else if(sign=="-"){
-                    let difference = parseInt(calculate[0])-parseInt(calculate[2]);
-                    screen.value = difference;
-                    screen.value += e;
-                }
-            }
-            
-        }
-    })
+        updateNum1(e);
+    });
 })
+
